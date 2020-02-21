f37aa682df5e6104628b9278746af282e65777fb
waka <tatsuhiro.0323@gmail.com>
2020-02-22 08:58:06 +0900

    相手色マーカ認識部追加

--- burger_war/scripts/EnemyState.py
+++ burger_war/scripts/EnemyState.py
@@ -5,7 +5,6 @@
 This is ALL SENSOR use node.
 Mainly echo sensor value in tarminal.
 Please Use for your script base.
-
 by Takuya Yamaguchi @dashimaki360
 '''
 
@@ -17,22 +16,21 @@ from std_msgs.msg import String
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 import tf
-
+import numpy as np
 
 class EnemyBot(object):
     def __init__(self, use_camera=False):
-        # カメラ画像上での赤マーカ位置
-        self.CP = [0,0]
+        # カメラ画像上での赤マーカ位置,サイズ
+        self.cam_Point_x = 0.0
+        self.cam_Point_y = 0.0
+        self.cam_Point_size = 0.0
         # velocity publisher
         self.relative_pose_pub = rospy.Publisher('relative_pose', PoseStamped ,queue_size=10)
-        # velocity publisher
-        #self.vel_pub = rospy.Publisher('cmd_vel', Twist,queue_size=1)
 
         # camera subscribver
         # please uncoment out if you use camera
         if use_camera:
             # for convert image topic to opencv obj
-            
             self.img = None
             self.bridge = CvBridge()
             self.image_sub = rospy.Subscriber('image_raw', Image, self.imageCallback)
@@ -46,8 +44,8 @@ class EnemyBot(object):
         while not rospy.is_shutdown():
             # update PoseStamped
             pose = PoseStamped()
-            pose.pose.position.x = self.CP[1]
-            pose.pose.position.y = self.CP[0]
+            pose.pose.position.x = 0
+            pose.pose.position.y = 0
             euler_z = 0 # dummy
 
             q = tf.transformations.quaternion_from_euler(0.0, 0.0, euler_z)
@@ -66,9 +64,49 @@ class EnemyBot(object):
         cv.InRangeS(imgHSV, cv.Scalar(150, 160, 60), cv.Scalar(200, 256, 256), imgResult)
         return imgResult
 
-    def ColorCenter(self,Image):
-        OP = [10,0]
-        return OP
+    def ColorCenter(self):
+#        self.img = cv2.rectangle(self.img, (0,360), (640,480), (0,0,0), -1)
+        hsv_img = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
+        color_min = np.array([0,100,150])
+        color_max = np.array([50,255,255])
+        color_mask = cv2.inRange(hsv_img, color_min, color_max)
+        bin_img = cv2.bitwise_and(self.img, self.img, mask = color_mask)
+        bin_img = cv2.cvtColor(bin_img, cv2.COLOR_BGR2GRAY)
+        nLabels, label_img, data, center = cv2.connectedComponentsWithStats(bin_img)
+        #print(nLabels)
+        if nLabels < 2:
+            return (0.0,0.0,0.0)
+        size_max = 0
+        size_max_x = 0
+        size_max_y = 0
+        size_max_w = 0
+        size_max_h = 0
+        center_max_x = 0
+        center_max_y = 0
+
+        for i in range(1, nLabels):
+            x, y, w, h, size = data[i]
+            center_x, center_y = center[i]
+            if size > size_max:
+                size_max_x = x
+                size_max_y = y
+                size_max_w = w
+                size_max_h = h
+                size_max = size
+    #            center_max_x = size_max_x + size_max_w/2
+    #            center_max_y = size_max_y + size_max_h/2
+                center_max_x = center_x
+                center_max_y = center_y
+
+        print(size_max)
+        if size_max < 30:
+            center_max_x = 0
+            center_max_y = 0
+            size_max = 0
+        print(size_max)
+        self.img = cv2.rectangle(self.img, (size_max_x, size_max_y), (size_max_x+size_max_w, size_max_y+size_max_h), (0, 255, 255), 3)        
+
+        return (center_max_x, center_max_y, size_max)
 
     # camera image call back sample
     # comvert image topic to opencv object and show
@@ -78,9 +116,9 @@ class EnemyBot(object):
         except CvBridgeError as e:
             rospy.logerr(e)
 
-        # 色重心        
-        self.CP = self.ColorCenter(self.img)
-        
+        # 色重心
+        self.cam_Point_x , self.cam_Point_y , self.cam_Point_size = self.ColorCenter()
+        print(self.cam_Point_x , self.cam_Point_y , self.cam_Point_size)
         cv2.imshow("Image window", self.img)
         cv2.waitKey(1)
 
@@ -88,5 +126,4 @@ if __name__ == '__main__':
     rospy.init_node('relative_enemy_camera')
     bot = EnemyBot(use_camera=True)
     bot.strategy()
-
 
