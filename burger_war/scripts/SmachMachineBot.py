393e05d0b889061a7cdde6c0d5eae07f78b3a060
yoshiki <yoshiki48690613@gmail.com>
2020-02-29 11:57:37 +0900

    add SmachMachineBot.py

--- burger_war/scripts/SmachMachineBot.py
+++ burger_war/scripts/SmachMachineBot.py
@@ -1,79 +1,97 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-import roslib; roslib.load_manifest('smach_tutorials')
 import rospy
 import smach
 import smach_ros
+import random
 
-# define state Foo
-class Foo(smach.State):
+
+class search_mode(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['outcome1','outcome2'])
-        self.counter = 0
+        smach.State.__init__(self, outcomes=['enemy','flag'],input_keys = ['search_input'],output_keys = ['search_output'])
 
-    def execute(self, userdata):
-        rospy.loginfo('Executing state FOO')
-        if self.counter < 3:
-            self.counter += 1
-            return 'outcome1'
+    def execute(self,userdata):
+        #search progarm enemy  or flag
+        rospy.loginfo('Executing state search_mode')
+        value = random.randint(1,1000)
+        print(value)
+        rospy.sleep(3)
+        if value < 500:
+            userdata.search_output = value
+            return 'enemy'
         else:
-            return 'outcome2'
+            userdata.search_output = value
+            return 'flag'
 
-
-# define state Bar
-class Bar(smach.State):
+class go_enemy(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['outcome1'])
+        smach.State.__init__(self,outcomes=['success'],
+        input_keys = ['go_enemy_input'],
+        output_keys = ['go_enemy_output'])
 
-    def execute(self, userdata):
-        rospy.loginfo('Executing state BAR')
-        return 'outcome1'
-        
+    def execute(self,userdata):
+        rospy.loginfo('Executing state go_enemy')
+        for index in range(userdata.go_enemy_input):
+            #rospy.sleep(0.1)
+            print('gogo')
 
+        return 'success'
 
-# define state Bas
-class Bas(smach.State):
+    
+class go_flag(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['outcome3'])
+        smach.State.__init__(self,outcomes=['enemy','search_mode'],
+        input_keys = ['go_flag_input'],
+        output_keys = ['go_flag_output'])
 
-    def execute(self, userdata):
-        rospy.loginfo('Executing state BAS')
-        return 'outcome3'
+    def execute(self,userdata):
+        rospy.loginfo('Executing state go_flag')
+        value = random.randint(1,1000)
+        print(value)
+        for index in range(userdata.go_flag_input):
+            print('gogo')
+            #rospy.sleep(0.1)
+        if value < 500 :
+            return 'enemy'
+        else:
+            return 'search_mode'
 
-
+    
 
 
 def main():
     rospy.init_node('smach_example_state_machine')
 
-    # Create the top level SMACH state machine
-    sm_top = smach.StateMachine(outcomes=['outcome5'])
-    
-    # Open the container
-    with sm_top:
+    sm = smach.StateMachine(outcomes = ['success'],
+    input_keys = ['sm_input'],
+    output_keys = ['sm_output'])
+    sm_input = 0
+    sm_output 
 
-        smach.StateMachine.add('BAS', Bas(),
-                               transitions={'outcome3':'SUB'})
+    with sm:
+        # Add states to the container
+        smach.StateMachine.add('search_mode', search_mode(), 
+                               transitions = {'enemy':'enemy', 'flag':'flag'},
+                                remapping = {'search_input':'sm_input',
+                                             'search_output':'sm_data'})
+        smach.StateMachine.add('enemy', go_enemy(), 
+                               transitions = {'success':'success'},
+                               remapping = {'go_enemy_input':'sm_data',
+                                            'go_enemy_output':'sm_output'})
+        smach.StateMachine.add('flag',go_flag(), 
 
-        # Create the sub SMACH state machine
-        sm_sub = smach.StateMachine(outcomes=['outcome4'])
+                               transitions={'enemy':'enemy','search_mode':'search_mode'},
+                               remapping = {'go_flag_input':'sm_data',
+                                            'go_flag_output':'sm_data'})
 
-        # Open the container
-        with sm_sub:
-
-            # Add states to the container
-            smach.StateMachine.add('FOO', Foo(), 
-                                   transitions={'outcome1':'BAR', 
-                                                'outcome2':'outcome4'})
-            smach.StateMachine.add('BAR', Bar(), 
-                                   transitions={'outcome1':'FOO'})
-
-        smach.StateMachine.add('SUB', sm_sub,
-                               transitions={'outcome4':'outcome5'})
+    # Create and start the introspection server
+    sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
+    sis.start()
 
     # Execute SMACH plan
-    outcome = sm_top.execute()
-
+    outcome = sm.execute()
+    rospy.spin()
 
 
 if __name__ == '__main__':
