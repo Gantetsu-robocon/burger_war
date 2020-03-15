aa9274308039d924ad206bb1639f9112d38d1d06
ryoya-suzuki <vbppos.rs.917@gmail.com>
2020-03-15 17:49:29 +0900

    一時的応急処置

--- burger_war/scripts/pub_enemy.py
+++ burger_war/scripts/pub_enemy.py
@@ -24,7 +24,7 @@ class PubEnemyPose():
         #Transformer, Listener, Subscriber, Publisher
         self.tfBuffer = tf2_ros.Buffer()
         self.listener = tf2_ros.TransformListener(self.tfBuffer)
-        self.enemy_pub = rospy.Publisher('absolute_pos', PoseStamped, queue_size=20)
+        self.enemy_pub = rospy.Publisher('absolute_pos', PoseStamped, queue_size=10)
         self.flag_sub = rospy.Subscriber('/color_flag_time', Int16MultiArray,self.flagcolorCallback)
         self.flag_sub = rospy.Subscriber('/lidar_flag', Bool, self.flaglidarCallback)
 
@@ -64,11 +64,13 @@ class PubEnemyPose():
         msg = PoseStamped()
         msg.header.frame_id = "map"
         if self.flag_lidar == True:
-            msg.pose = self.t_lidar.pose
+            msg.pose.position = self.t_lidar.pose.position
+            msg.pose.orientation = self.t_lidar.pose.orientation
             self.enemy_pub.publish(msg)
             #rospy.loginfo("Publish Lidar pose")
         elif self.flag_color == True:
-            msg.pose = self.t_camera.pose
+            msg.pose.position = self.t_camera.pose.position
+            msg.pose.orientation = self.t_camera.pose.orientation
             self.enemy_pub.publish(msg)
             #rospy.loginfo("Publish Camera pose")
 
@@ -87,10 +89,11 @@ class PubEnemyPose():
         while not rospy.is_shutdown():
             self.lisn_enemy_camera()
             self.lisn_enemy_lidar()
-            self.pub_enemy_abs()
+            if (self.flag_color==True) or (self.flag_lidar==True):
+                self.pub_enemy_abs()
             rate.sleep()
 
 if __name__ == '__main__':
-    rospy.init_node('pub_enemy_abs')
+    rospy.init_node('pub_enemy')
     pub_enemy = PubEnemyPose()
     pub_enemy.main()
